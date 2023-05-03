b02b4660425f2a2a8453a63795947cb8abe6c2f5
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2023-05-04 00:29:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
 gdown "https://drive.google.com/uc?export=download&id=1-DLdSsCbvoDvdlCV2iE1NwrTARezZbKz" -O mask_check.pt
-streamlit run mask_check.py --server.port 8888
+streamlit run mask_check.py --server.port 10000
